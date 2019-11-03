542c1af8e2723738188f7b185eda4fe99d5915ec
Johannes Weber <mail@johannesweber.me>
2019-11-03 21:04:24 +0800

    Cleaning code

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,14 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
 //= link components/hs.header.js
 //= link components/hs.scroll-nav.js
-//= link vendor/svg-injector/dist/svg-injector.min.js
-//= link vendor/fancybox/jquery.fancybox.min.js
+
 //= link components/hs.svg-injector.js
+//= link vendor/svg-injector/dist/svg-injector.min.js
+
+//= link vendor/fancybox/jquery.fancybox.min.js
 //= link components/hs.fancybox.js
 
+//= link vendor/jquery/dist/jquery.min.js
