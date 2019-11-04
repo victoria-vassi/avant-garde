9ca8dc15f9fcac663688297ade363cb71e84178c
Pablo Haguet <pablo.haguet@skema.edu>
2019-11-04 17:11:42 +0800

    First commit for Investments and Campaigns methods

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,13 +1,31 @@
 class InvestmentsController < ApplicationController
   def index
+    @investments = Investment.all
   end
 
   def show
+    @investment = Investment.find(params[:id])
   end
 
   def new
+    @investment = Investment.new
+    @campaign = Campaign.find(params[:campaign_id])
+    authorize @campaign
+    authorize @investment
   end
 
   def create
+    @investment = Investment.new
+    @campaign = Campaign.find(params[:campaign_id])
+    @user = current_user
+    authorize @campaign
+    @investment.campaign = @campaign
+    @investment.user = @user
+    if @investment.save
+      redirect_to dashboard_path(@user)
+    else
+      @investments = Investment.where("campaign_id = '#{params[:campaign_id]}'")
+      render 'campaigns/show'
+    end
   end
 end
