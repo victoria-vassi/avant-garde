93669a27f078b73d89a2dd2adc8992be34447918
Johannes Weber <mail@johannesweber.me>
2019-11-05 16:35:42 +0800

    Added links to sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'investments/index'
-  get 'investments/show'
-  get 'investments/new'
-  get 'investments/create'
-  get 'campaigns/index'
-  get 'campaigns/show'
-  get 'campaigns/new'
-  get 'campaigns/create'
   devise_for :users
   root to: 'pages#home'
+
+  get "profiles/:id", to: "pages#user_profile", as: :user_profile
 
   get "/dashboard", to: 'pages#dashboard', as: :dashboard
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
