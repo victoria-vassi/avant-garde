3d60ac5e89c10ee00f9da4f246590d688e7261da
Pablo Haguet <pablo.haguet@skema.edu>
2019-11-07 21:28:28 +0800

    Added Status to Investments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_050354) do
+ActiveRecord::Schema.define(version: 2019_11_07_132705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_050354) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "date"
+    t.boolean "status"
     t.index ["campaign_id"], name: "index_investments_on_campaign_id"
     t.index ["user_id"], name: "index_investments_on_user_id"
   end
