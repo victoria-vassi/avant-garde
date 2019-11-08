ca33d9786f7f86fe645a2fad2532d6e4f568fb55
victoria vassileva <victoria.vassileva@yahoo.fr>
2019-11-08 14:11:33 +0800

    Styling of the checkout process with Stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,25 @@
 class OrdersController < ApplicationController
   def create
     campaign = Campaign.find(params[:campaign])
-    order = Order.create!(campaign: campaign, amount: campaign.price, user: current_user)
+    @order = Order.create!(campaign: campaign, amount: campaign.price, user: current_user)
 
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
         name: campaign.title,
-        amount: order.amount_cents / 100,
+        amount: @order.amount_cents / 100,
         currency: 'usd',
         quantity: 1
       }],
-      success_url: order_url(order),
-      cancel_url: order_url(order)
+      success_url: order_url(@order),
+      cancel_url: order_url(@order)
     )
 
-    order.update(checkout_session_id: session.id)
-    redirect_to new_order_payment_path(order)
+    @order.update(checkout_session_id: session.id)
+    respond_to do |format|
+      format.html { redirect_to new_order_payment_path(@order) }
+      format.js
+    end
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
 // import "bootstrap";
 import { subnav } from '../components/subnav';
 import { formAmount } from '../components/formamount'
+import {stepProgressbar } from '../components/stepprogressbar'
 
 subnav();
 formAmount();
-
-
+// stepprogressbar();
