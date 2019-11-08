053fa5b70a721b8fc221780d6bd92d7190eae08b
Johannes Weber <mail@johannesweber.me>
2019-11-08 13:20:49 +0800

    Added first elements to dashboard overviwe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,20 @@ class PagesController < ApplicationController
 
   def dashboard
     @investments_user = Investment.where("user_id = ?", current_user.id).order("date DESC")
+
+    @total_amount = 0
+    @investments_user.each do |investment|
+      @total_amount += investment.amount
+    end
+
     @campaigns_user = Campaign.joins(:investments).where("user_id = ?", current_user.id).order("end_date DESC").uniq
+   
+    @campaigns_user_closed = @campaigns_user.select do |campaign|
+      campaign.funded == true
+    end
+    @campaigns_user_open = @campaigns_user.select do |campaign|
+      campaign.funded == false
+    end
   end
 
   def user_profile
