49d35f6e390ceacb99fe0bd960d87dedb8121576
Pablo Haguet <pablo.haguet@skema.edu>
2019-11-11 12:13:11 +0800

    trying to solve if else statement for confirmation modal

--- app/javascript/packs/confinvestment.js
+++ app/javascript/packs/confinvestment.js
@@ -1,18 +1,30 @@
 const sliderInput = document.querySelector("#test")
-  sliderInput.addEventListener("click", () => {
-    const listenedAmount = document.querySelector(".irs-single")
-    const array = listenedAmount.innerText.split("")
-    const newArray = array.filter(function(str) {
+sliderInput.addEventListener("click", () => {
+const listenedAmount = document.querySelector(".irs-single")
+const array = listenedAmount.innerText.split("")
+const newArray = array.filter(function(str) {
   return /\S/.test(str);
   });
-    const amount = newArray.join("")
-    const campaignPrice = sliderInput.dataset.price
-    const div = parseInt((parseInt(amount) / parseInt(campaignPrice) * 100))
+const amount = newArray.join("")
+const campaignPrice = sliderInput.dataset.price
+const campaignMinInvestment = sliderInput.dataset.min
+const min = parseInt(campaignMinInvestment)
+const div = parseInt((parseInt(amount) / parseInt(campaignPrice) * 100))
 
-    const investedAmount = document.querySelector("#invested_amount_conf")
- investedAmount.innerText = `You want to invest $${amount}`
-    const investedShare = document.querySelector("#invested_share_conf")
+const investedShare = document.querySelector("#invested_share_conf")
+const investedAmount = document.querySelector("#invested_amount_conf")
 
-    investedShare.innerText = `You will own ${div}% of the art piece`
+    if (parseInt(amount) === min) {
+      investedAmount.innerText = `You want to invest $${campaignMinInvestment}`
+      investedShare.innerText = `You will own ${div}% of the art piece`
+    } else {
+      investedAmount.innerText = `You want to invest $${amount}`
+      investedShare.innerText = `You will own ${div}% of the art piece`
+    }
 });
+
+
+
+
+
 
