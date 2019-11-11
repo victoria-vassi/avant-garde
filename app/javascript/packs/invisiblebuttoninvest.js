c5a32fe0f5787ca032c4c336780ed40bd58c6252
Pablo Haguet <pablo.haguet@skema.edu>
2019-11-11 15:24:39 +0800

    Added javascript for changing value and disabling button when campaign closed

--- app/javascript/packs/invisiblebuttoninvest.js
+++ app/javascript/packs/invisiblebuttoninvest.js
@@ -1,5 +1,8 @@
-  const buttonInvest = document.querySelector("#button-invest").dataset.check
-if (buttonInvest === true) {
-  buttonInvest.hide();
+  const buttonInvest = document.querySelector("#button-invest")
+if (buttonInvest.dataset.funded === "true") {
+  buttonInvest.classList.remove("btn-primary");
+  buttonInvest.classList.add("btn-dark");
+  buttonInvest.value = "Campaign closed";
+  buttonInvest.disabled = true;
 }
 
