bdaf23b499b493271f6c15e32d7d3881713779a3
victoria vassileva <victoria.vassileva@yahoo.fr>
2019-11-11 15:44:27 +0800

    PDF is working

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -7,22 +7,6 @@ class Certificate < ApplicationRecord
     :closed => "badge badge-secondary"
   }
 
-  # def subtotal
-  #   self.invoice_items.map { |item| item.qty * item.price }.sum
-  # end
-
-  # def discount_calculated
-  #   subtotal * (self.discount / 100.0)
-  # end
-
-  # def vat_calculated
-  #   (subtotal - discount_calculated) * (self.vat / 100.0)
-  # end
-
-  # def total
-  #   subtotal - discount_calculated + vat_calculated
-  # end
-
   def status_class
     STATUS_CLASS[self.status]
   end
