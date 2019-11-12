4769e5a030c1670a57ef5e6e10dbbe050d72954d
Pablo Haguet <pablo.haguet@skema.edu>
2019-11-12 10:52:17 +0800

    for pulling master

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,7 +2,7 @@ class Investment < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
   validates :amount, presence: true
-  validates :status, presence: true
+  validates :status, default: false
 end
 
 
