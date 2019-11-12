1dd9b2f51beec2f95fd897b28ed14f1ec63c2522
Pablo Haguet <pablo.haguet@skema.edu>
2019-11-12 19:07:01 +0800

    Updated funding_status after payment

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -3,16 +3,4 @@ class Investment < ApplicationRecord
   belongs_to :campaign
   has_one :certificate
   validates :amount, presence: true
-
-  def update_after_paid
-    current_funding = campaign.price*campaign.funding_status/100
-    current_funding += amount
-    new_funding_status = (current_funding.to_f/campaign.price.to_f*100).to_i
-    campaign.funding_status = new_funding_status
-    if new_funding_status >= 100
-      campaign.funded = true
-      campaign.funding_status = 100
-    end
-    campaign.save!
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_051246) do
+ActiveRecord::Schema.define(version: 2019_11_11_043842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,7 +97,6 @@ ActiveRecord::Schema.define(version: 2019_11_11_051246) do
     t.string "amount_currency", default: "USD", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "investment_id"
     t.index ["campaign_id"], name: "index_orders_on_campaign_id"
     t.index ["investment_id"], name: "index_orders_on_investment_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
