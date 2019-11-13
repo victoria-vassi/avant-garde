7fce482193395d4e15e3105d22e40bec28504e50
Pablo Haguet <pablo.haguet@skema.edu>
2019-11-13 11:26:52 +0800

    Fixed amount and percentage format in modal

--- app/javascript/packs/confinvestment.js
+++ app/javascript/packs/confinvestment.js
@@ -1,32 +1,27 @@
 const sliderInput = document.querySelector("#test")
 
 const addValues = () => {
-  Number.prototype.number_with_delimiter = function(delimiter) {
-    var number = this + '', delimiter = delimiter || ',';
-    var split = number.split('.');
-    split[0] = split[0].replace(
-        /(\d)(?=(\d\d\d)+(?!\d))/g,
-        '$1' + delimiter
-    );
-    return split.join('.');
-};
+
   const listenedAmount = document.querySelector(".irs-single")
   const array = listenedAmount.innerText.split("")
   const newArray = array.filter(function(str) {
     return /\S/.test(str);
     });
-  const amount = newArray.join("")
+  const amount1 = newArray.join("")
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+  const amount = numberWithCommas(amount1)
   const campaignPrice = sliderInput.dataset.price
   const campaignMinInvestment = sliderInput.dataset.min
   const min = parseInt(campaignMinInvestment)
-  const div1 = parseInt(amount) / parseInt(campaignPrice) * 100
+  const div1 = parseInt(amount1) / parseInt(campaignPrice) * 100
   const div = Math.round( div1 * 10 ) / 10;
 
   const investedShare = document.querySelector("#invested_share_conf")
   const investedAmount = document.querySelector("#invested_amount_conf")
 
-  investedAmount.innerText = `You want to invest $${amount}`
-  investedShare.innerText = `You will own ${div}% of the art piece`
+  investedAmount.innerText = `$${amount}, ie ${div}% of the art piece`
 }
 
 setTimeout(addValues, 500)
@@ -38,19 +33,21 @@ setTimeout(addValues, 500)
     const newArray = array.filter(function(str) {
       return /\S/.test(str);
       });
-    const amount = newArray.join("")
+    const amount1 = newArray.join("")
+      function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+    const amount = numberWithCommas(amount1)
     const campaignPrice = sliderInput.dataset.price
     const campaignMinInvestment = sliderInput.dataset.min
     const min = parseInt(campaignMinInvestment)
-    const div1 = parseInt(amount) / parseInt(campaignPrice) * 100
+    const div1 = parseInt(amount1) / parseInt(campaignPrice) * 100
     const div = Math.round( div1 * 10 ) / 10;
 
     const investedShare = document.querySelector("#invested_share_conf")
     const investedAmount = document.querySelector("#invested_amount_conf")
 
-    investedAmount.innerText = `You want to invest $${amount}`
-    investedShare.innerText = `You will own ${div}% of the art piece`
-
+    investedAmount.innerText = `$${amount}, ie ${div}% of the art piece`
   });
 
 
