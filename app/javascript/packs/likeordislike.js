6bb3face9962ee4404703c995379ff78a4b8bb25
Peter Suh <psuh22@gmail.com>
2019-11-14 12:10:40 +0800

    last changes

--- app/javascript/packs/likeordislike.js
+++ app/javascript/packs/likeordislike.js
@@ -2,23 +2,25 @@ const thumbsup = document.querySelectorAll("#thumbsup")
 const thumbsdown = document.querySelectorAll("#thumbsdown")
 // const likebuttoncounter = document.querySelector("#ourcounter")
 
+
+const test = (e) => {
+  const count = e.target.parentElement.firstElementChild
+  var num = parseInt(count.innerText, 10)
+  num = num + 1
+  count.innerText = num.toString();
+  e.target.removeEventListener('click', test, false);
+
+}
+
 thumbsup.forEach((thumb) => {
-  thumb.addEventListener('click', (event) => {
-    const count = thumb.parentElement.firstElementChild
-    var num = parseInt(count.innerText, 10)
-    num = num + 1
-    count.innerText = num.toString();
-  });
-})
+  thumb.addEventListener('click', test)
+});
+
 
 thumbsdown.forEach((thumb) => {
-  thumb.addEventListener('click', (event) => {
-    const count = thumb.parentElement.firstElementChild
-    var num = parseInt(count.innerText, 10)
-    num = num + 1
-    count.innerText = num.toString();
-  });
-})
+  thumb.addEventListener('click', test)
+});
+
 
 
 
